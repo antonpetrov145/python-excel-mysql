64134acd7be132c6c0ae3b3f7714c4cda440a390
Tony <63195157+antonpetrov145@users.noreply.github.com>
2021-01-27 15:37:25 +0200

    Update excel.py

--- excel.py
+++ excel.py
@@ -7,16 +7,10 @@ cursor = db.cursor()
 wb = openpyxl.load_workbook('file.xlsx')
 ws = wb['sheet']
 
-rows = []
-
-for row in ws.iter_rows(min_row=1, min_col=1, values_only=True):
-    number = ws.cell(1,1).value
-    chip = ws.cell(1,3).value
-    rows.append((number, chip))
-
-db = MySQLdb.connect(host='host', user='user', db='testdb', passwd='passwd')
-cursor = db.cursor()
-cursor.executemany("""INSERT INTO table (col1,col2) VALUES (%s,%s)""", (rows))
+for i in range(2, ws.max_row+1):
+    row = [cell.value for cell in ws[i]]
+    #print (row)
+    cursor.execute("""INSERT INTO table (num,chip) VALUES (%s,%s)""", (str(row[1]), str(row[2]), ))
 
 db.commit()
 db.close()
